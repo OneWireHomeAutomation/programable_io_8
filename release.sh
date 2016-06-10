1d0b785d822f23e5b40eadd6cd0167692c0a5664
Christian Aurich <christian.aurich@c-aurich.de>
2016-06-10 22:09:17 +0200

    removing absolute paths

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python /home/christian/projects/eagle-bom.py/eagle-bom.py -i pio8.brd -o pio8_stickerbom.pdf -t sticker
-python /home/christian/projects/eagle-bom.py/eagle-bom.py -i pio8.brd -o pio8_bom.csv -t value
+python ./eagle-bom.py -i pio8.brd -o pio8_stickerbom.pdf -t sticker
+python ./eagle-bom.py -i pio8.brd -o pio8_bom.csv -t value
 zip pio8.zip pio8.sch pio8.brd pio8_stickerbom.pdf pio8_bom.csv
